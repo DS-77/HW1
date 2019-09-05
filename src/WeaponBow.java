5b8ba8ddccf4338d268d6019e2894516ffad3f14
Deja S <vaultd43@gmail.com>
2019-09-05 15:47:38 -0400

    Added the weaponBow class and implemented the interface method attack. Added comments to all classes.

---
+++ src/WeaponBow.java
@@ -0,0 +1,9 @@
+public class WeaponBow implements WeaponBehavior {
+    /**
+     * This method prints out the attack behavior of the weapon: Bow.
+     */
+    @Override
+    public void attack() {
+        System.out.println("Draw and loose an arrow");
+    }
+}
--- src/character.java
+++ src/character.java
@@ -1,23 +1,32 @@
 public abstract class character {
 
     protected String name;
-
-//    TODO: Implement this class.
     protected WeaponBehavior weaponBehavior;
 
-//    Parameterized Constructor
+    /**
+     * This method is the parameterized constructor for the Character class.
+     * @param name is the string that will be set as the character's name.
+     */
     public character( String name) {
         this.name = name;
-//        TODO: Don't forget to set weapon here.
     }
 
+    /**
+     * This method calls the WeaponBehavior class's attack method.
+     */
     public void attack() {
-//        TODO: Use the method provided by the weapon.
+        weaponBehavior.attack();
     }
 
-//    This method needs to be implemented by the children.
+    /**
+     * This method is defined by the children classes.
+     */
     public abstract void display ();
 
+    /**
+     * This method changes the type of weaponBehavior the character has.
+     * @param wb is the weaponBehavior object to be set as the character object's weaponBehavior.
+     */
     public void setWeaponBehavior(WeaponBehavior wb) {
         this.weaponBehavior = wb;
     }
