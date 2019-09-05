f2e9885207b929cf8237b83175069fa386e0b26e
Deja S <vaultd43@gmail.com>
2019-09-05 15:29:16 -0400

    Created the weaponSword class and implemented the interface attack method.

---
+++ src/WeaponSword.java
@@ -0,0 +1,9 @@
+public class WeaponSword implements WeaponBehavior{
+    /**
+     * This method prints out the attack behavior of the weapon: Sword.
+     */
+    @Override
+    public void attack() {
+        System.out.println("Lunge and strike with sword");
+    }
+}
