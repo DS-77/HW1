a3d0af6b47ae2875bfadde8ea8b098db37da2556
Deja S <vaultd43@gmail.com>
2019-09-05 14:56:09 -0400

    Create the character abstract class. Added constructor, proterties and a method.

---
+++ src/character.java
@@ -0,0 +1,21 @@
+public abstract class character {
+
+    protected String name;
+
+//    TODO: Implement this class.
+//    protected WeaponBehavior weaponBehavior;
+
+//    Parameterized Constructor
+    public character( String name) {
+        this.name = name;
+//        TODO: Don't forget to set weapon here.
+    }
+
+    public void attack() {
+//        TODO: Use the method provided by the weapon.
+    }
+
+//    This method needs to be implemented by the children.
+    public abstract void display ();
+}
+
