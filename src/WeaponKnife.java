99c99840231f1e2def908581829fe3f9f5048897
Deja S <vaultd43@gmail.com>
2019-09-05 15:31:35 -0400

    Created the weaponKnife class and implemented the interface method attack.

---
+++ src/WeaponKnife.java
@@ -0,0 +1,9 @@
+public class WeaponKnife implements WeaponBehavior {
+    /**
+     * This method prints out the attack behavior of the weapon: Knife.
+     */
+    @Override
+    public void attack() {
+        System.out.println("Slice with knife");
+    }
+}
