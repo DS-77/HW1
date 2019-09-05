a7093521bde112f864bd0fe59208550d1dc769ac
Deja S <vaultd43@gmail.com>
2019-09-05 15:18:45 -0400

    Added the weaponAxe class and implemented the interface method "attack".

---
+++ src/WeaponAxe.java
@@ -0,0 +1,9 @@
+public class WeaponAxe implements WeaponBehavior{
+    /**
+     * This method prints out the attack behavior of the weapon: Axe.
+     */
+    @Override
+    public void attack() {
+        System.out.println("Swing an axe");
+    }
+}
