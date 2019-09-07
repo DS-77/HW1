bb3227bb7f86cbab698b14bfb800a5070e69c7bb
Deja S <vaultd43@gmail.com>
2019-09-06 20:21:32 -0400

    Added header comment to each file created.

--- src/WeaponBehavior.java
+++ src/WeaponBehavior.java
@@ -1,4 +1,14 @@
+/**
+ * Author: Deja Scott
+ * Professor: Plante
+ * Section: CSCE 247 001
+ * Date: September 06 2019
+ * PURPOSE: This program implements the Strategy Design Pattern to create the characters and weapons in a Medieval Game.
+ */
+
 public interface WeaponBehavior {
-//    This method will be defined by the class that implements this interface.
-    public void attack ();
+    /**
+     *  This method will be defined by the class that implements this interface.
+     */
+    void attack();
 }
--- src/WeaponNone.java
+++ src/WeaponNone.java
@@ -1,3 +1,11 @@
+/**
+ * Author: Deja Scott
+ * Professor: Plante
+ * Section: CSCE 247 001
+ * Date: September 06 2019
+ * PURPOSE: This program implements the Strategy Design Pattern to create the characters and weapons in a Medieval Game.
+ */
+
 public class WeaponNone implements WeaponBehavior {
 
     /**
